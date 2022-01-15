2c63013f27890daff161350ffd8031666b72df58
Alex Ferm <abferm@gmail.com>
2022-01-15 13:25:42 -0600

    added some simple arithmatic functions

---
+++ gmath/arithmatic.go
@@ -0,0 +1,27 @@
+package gmath
+
+import "github.com/abferm/go-generics"
+
+// Add casts y to the type of x and returns the sum
+// cast to type Z
+func Add[X, Y, Z generics.Number](x X, y Y) Z {
+	return Z(x + X(y))
+}
+
+// Sub casts y to the type of x and returns x-y
+// cast to type Z
+func Sub[X, Y, Z generics.Number](x X, y Y) Z {
+	return Z(x - X(y))
+}
+
+// Mul casts y to the type of x and returns x*y
+// cast to type Z
+func Mul[X, Y, Z generics.Number](x X, y Y) Z {
+	return Z(x * X(y))
+}
+
+// Div casts y to the type of x and returns x/y
+// cast to type Z
+func Div[X, Y, Z generics.Number](x X, y Y) Z {
+	return Z(x / X(y))
+}
