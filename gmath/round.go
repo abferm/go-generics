8f86fc257c959de49f12ab70120bc68f7430991a
Alex Ferm <abferm@gmail.com>
2022-01-17 22:24:27 -0600

    added Trunc and Round wrappers that will operate on float32 or float64

--- gmath/round.go
+++ gmath/round.go
@@ -25,3 +25,23 @@ func Floor[F generics.Float](x F) F {
 func Ceil[F generics.Float](x F) F {
 	return F(math.Ceil(float64(x)))
 }
+
+// Trunc returns the integer value of x.
+//
+// Special cases are:
+//	Trunc(±0) = ±0
+//	Trunc(±Inf) = ±Inf
+//	Trunc(NaN) = NaN
+func Trunc[F generics.Float](x F) F {
+	return F(math.Trunc(float64(x)))
+}
+
+// Round returns the nearest integer, rounding half away from zero.
+//
+// Special cases are:
+//	Round(±0) = ±0
+//	Round(±Inf) = ±Inf
+//	Round(NaN) = NaN
+func Round[F generics.Float](x F) F {
+	return F(math.Round(float64(x)))
+}
--- gmath/sign.go
+++ gmath/sign.go
@@ -9,9 +9,10 @@ import (
 // Sign returns a value with the sign
 // of x and a magnitude of 1.
 func Sign[X generics.SignedNumber](x X) X {
+	// We can't do type assertions or switches on generics, so cast to empty interface
 	switch interface{}(x).(type) {
 	case float32, float64:
-		// special case to get the sign of NaN and Zero
+		// special case to get the sign of any float, including NaN and Zero
 		if math.Signbit(float64(x)) {
 			return -1
 		}
