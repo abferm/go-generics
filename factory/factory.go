57787c89e87c617c480f289d4797216cbacb703a
Alex Ferm <abferm@gmail.com>
2022-01-17 22:53:59 -0600

    factory improvements
     * Factory and ConfiguredFactory now take in context.Context
     * Added ContextWithFactory and FromContext for registering and
    retreiving a ConfituredFactory on a context.Context
     * Added more tests besides the ExampleTest

--- factory/factory.go
+++ factory/factory.go
@@ -1,6 +1,7 @@
 package factory
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/abferm/go-generics"
@@ -8,11 +9,11 @@ import (
 
 type ReleaseFunc = func() error
 
-type Factory[CONFIG any, TARGET any] func(CONFIG) (TARGET, ReleaseFunc, error)
-type ConfiguredFactory[TARGET any] func() (TARGET, ReleaseFunc, error)
+type Factory[CONFIG any, TARGET any] func(context.Context, CONFIG) (TARGET, ReleaseFunc, error)
+type ConfiguredFactory[TARGET any] func(context.Context) (TARGET, ReleaseFunc, error)
 
 func NewSingletonFactory[TARGET any](singleton TARGET) ConfiguredFactory[TARGET] {
-	return func() (TARGET, ReleaseFunc, error) {
+	return func(context.Context) (TARGET, ReleaseFunc, error) {
 		return singleton, func() error { return nil }, nil
 	}
 }
@@ -38,24 +39,51 @@ func (r Registry[ID, CONFIG, TARGET]) Deregister(id ID) {
 	delete(r, id)
 }
 
-func (r Registry[ID, CONFIG, TARGET]) Build(id ID, config CONFIG) (TARGET, ReleaseFunc, error) {
+func (r Registry[ID, CONFIG, TARGET]) Build(ctx context.Context, id ID, config CONFIG) (TARGET, ReleaseFunc, error) {
 	// zil is a zero value for the target to return in case of an error
 	zil := generics.Zero[TARGET]()
 	f, ok := r[id]
 	if !ok {
-		return zil, nil, fmt.Errorf("No factory registered for ID %v", id)
+		return zil, nil, fmt.Errorf("no factory registered for ID %v", id)
 	}
 
-	return f(config)
+	return f(ctx, config)
 }
 
 func (r Registry[ID, CONFIG, TARGET]) GetConfiguredFactory(id ID, config CONFIG) (ConfiguredFactory[TARGET], error) {
 	f, ok := r[id]
 	if !ok {
-		return nil, fmt.Errorf("No factory registered for ID %v", id)
+		return nil, fmt.Errorf("no factory registered for ID %v", id)
 	}
 
-	return func() (TARGET, ReleaseFunc, error) {
-		return f(config)
+	return func(ctx context.Context) (TARGET, ReleaseFunc, error) {
+		return f(ctx, config)
 	}, nil
+}
+
+type contextKey[TARGET any] struct{}
+
+// ContextWithFactory attaches a ConfiguredFactory to the context to be passed to resources that use FromContext
+// 			Incase of an error, the returned context will be the provided context, it is always safe to overwrite
+//			the provided context with the resulting context.
+func ContextWithFactory[TARGET any](ctx context.Context, factory ConfiguredFactory[TARGET]) (context.Context, error) {
+	val := ctx.Value(contextKey[TARGET]{})
+	if val != nil {
+		return ctx, fmt.Errorf("%T already registered on context", factory)
+	}
+	return context.WithValue(ctx, contextKey[TARGET]{}, factory), nil
+}
+
+// FromContext fetches the ConfiguredFactory stored on the context
+func FromContext[TARGET any](ctx context.Context) (ConfiguredFactory[TARGET], error) {
+	var f ConfiguredFactory[TARGET]
+	val := ctx.Value(contextKey[TARGET]{})
+	if val == nil {
+		return nil, fmt.Errorf("no %T registered on context", f)
+	}
+	f, ok := val.(ConfiguredFactory[TARGET])
+	if !ok {
+		return nil, fmt.Errorf("value for %T was not a %T: %T", contextKey[TARGET]{}, f, val)
+	}
+	return f, nil
 }
--- factory/factory_test.go
+++ factory/factory_test.go
@@ -1,9 +1,12 @@
 package factory_test
 
 import (
+	"context"
 	"fmt"
+	"testing"
 
 	"github.com/abferm/go-generics/factory"
+	"github.com/stretchr/testify/assert"
 )
 
 func ExampleFactory() {
@@ -22,19 +25,75 @@ func ExampleFactory() {
 	err = registry.Register("icecream", buildIcecream)
 	fmt.Println(err)
 
-	myPizza, _, err := registry.Build("pizza", []string{"cheese", "pepperoni", "sausage"})
+	myPizza, _, err := registry.Build(context.Background(), "pizza", []string{"cheese", "pepperoni", "sausage"})
 	fmt.Println(myPizza.Describe(), err)
 
-	myIcecream, _, err := registry.Build("icecream", []string{"chocolate"})
+	myIcecream, _, err := registry.Build(context.Background(), "icecream", []string{"chocolate"})
 	fmt.Println(myIcecream.Describe(), err)
 
-	_, _, err = registry.Build("icecream", []string{"chocolate", "vanilla"})
+	_, _, err = registry.Build(context.Background(), "icecream", []string{"chocolate", "vanilla"})
 	fmt.Println(err)
 
 	// Output: factory already registered for icecream
 	// Pizza piled high with: [cheese pepperoni sausage] <nil>
 	// Sweet chocolate icecream <nil>
 	// our icecream must have exactly one flavor
+}
+
+func TestRegistry(t *testing.T) {
+	registry := factory.NewRegistry[string, []string, food]()
+
+	err := registry.Register("pizza", buildPizza)
+	assert.NoError(t, err)
+
+	err = registry.Register("icecream", buildIcecream)
+	assert.NoError(t, err)
+
+	_, _, err = registry.Build(context.Background(), "pizza", []string{"cheese", "pepperoni", "sausage"})
+	assert.NoError(t, err)
+
+	registry.Deregister("pizza")
+	_, _, err = registry.Build(context.Background(), "pizza", []string{"cheese", "pepperoni", "sausage"})
+	assert.EqualError(t, err, "no factory registered for ID pizza", "Pizza factory should have been removed from the registry")
+	_, err = registry.GetConfiguredFactory("pizza", []string{"cheese", "pepperoni", "sausage"})
+	assert.EqualError(t, err, "no factory registered for ID pizza", "Pizza factory should have been removed from the registry")
+
+}
+
+func TestContextWithFactory(t *testing.T) {
+	registry := factory.NewRegistry[string, []string, food]()
+
+	err := registry.Register("pizza", buildPizza)
+	assert.NoError(t, err)
+
+	err = registry.Register("icecream", buildIcecream)
+	assert.NoError(t, err)
+
+	favoriteFood, err := registry.GetConfiguredFactory("pizza", []string{"cheese", "pepperoni", "sausage"})
+	assert.NoError(t, err)
+	ctx, err := factory.ContextWithFactory(context.Background(), favoriteFood)
+	assert.NoError(t, err)
+
+	f, err := factory.FromContext[food](ctx)
+	assert.NoError(t, err)
+
+	food1, _, _ := favoriteFood(context.Background())
+	food2, _, _ := f(context.Background())
+	assert.Equal(t, food1, food2, "Expected the same food")
+
+	favoriteDesert, err := registry.GetConfiguredFactory("icecream", []string{"peach"})
+	assert.NoError(t, err)
+
+	ctx, err = factory.ContextWithFactory(ctx, favoriteDesert)
+	assert.EqualError(t, err, "factory.ConfiguredFactory[factory.food] already registered on context")
+
+	f, err = factory.FromContext[food](ctx)
+	assert.NoError(t, err)
+	food2, _, _ = f(context.Background())
+	assert.Equal(t, food1, food2, "stored factory should not have changed")
+
+	_, err = factory.FromContext[int](ctx)
+	assert.EqualError(t, err, "no factory.ConfiguredFactory[int] registered on context")
 }
 
 type food interface {
@@ -47,7 +106,7 @@ type pizza struct {
 	toppings []string
 }
 
-func buildPizza(toppings []string) (food, func() error, error) {
+func buildPizza(_ context.Context, toppings []string) (food, func() error, error) {
 	return &pizza{toppings}, func() error { return nil }, nil
 }
 
@@ -67,7 +126,7 @@ type icecream struct {
 	flavor string
 }
 
-func buildIcecream(flavor []string) (food, func() error, error) {
+func buildIcecream(_ context.Context, flavor []string) (food, func() error, error) {
 	if len(flavor) != 1 {
 		return nil, nil, fmt.Errorf("our icecream must have exactly one flavor")
 	}
